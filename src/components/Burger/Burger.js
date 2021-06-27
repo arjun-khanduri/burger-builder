ef537ba40c209d49950e1df83cdda8daf17b5589
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-06-27 15:52:08 +0530

    Added message for user to add ingredients in case none are added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,18 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(key => {
             return [...Array(props.ingredients[key])].map((_, i) => {
                 return <BurgerIngredient key={key + i} type={key} />
             });
-        });
-
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el); 
+        }, [])
+        if(transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients</p>;
+        }
     return (
         <div className='Burger'>
             <BurgerIngredient type='bread-top' />
