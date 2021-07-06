f61b091d7d8d0f53e79ae840e943d71d12db08db
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-06 15:59:52 +0530

    Added the dummy orders component

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () => (
     <ul className="NavigationItems">
         <NavigationItem link="/">Burger Builder</NavigationItem>
-        <NavigationItem link="/" active>Checkout</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
 );
 
