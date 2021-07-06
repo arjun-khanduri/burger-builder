c584225bee2974729cc36ae3180ee847d8c981c5
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-06 16:52:22 +0530

    Created input component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import './ContactData.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends React.Component {
     state = {
@@ -46,10 +47,10 @@ class ContactData extends React.Component {
     render() {
         let form = (
             <form>
-                <input className="Input" type="text" name="name" placeholder="Your name" />
-                <input className="Input" type="email" name="email" placeholder="Your email" />
-                <input className="Input" type="text" name="street" placeholder="Street" />
-                <input className="Input" type="text" name="postal" placeholder="Postal Code" />
+                <Input inputtype="input" type="text" name="name" placeholder="Your name" />
+                <Input inputtype="input" type="email" name="email" placeholder="Your email" />
+                <Input inputtype="input" type="text" name="street" placeholder="Street" />
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
