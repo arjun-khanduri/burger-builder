85876ed94a0829bdc942b1d69b19f27c9c7b9d3e
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-06 16:25:52 +0530

    Displaying fetched data

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './Order.css';
 
-const order = (props) => (
-    <div className='Order'>
-        <p>Ingredients: Salad(1)</p>
-        <p>Price <strong>5.45</strong></p>
-    </div>
-);
+const order = (props) => {
+    const ingredients = [];
+
+    for (let ingredient in props.ingredients) {
+        ingredients.push(
+            {
+                name: ingredient,
+                amount: props.ingredients[ingredient]
+            }
+        );
+    }
+
+    const ingredientOutput = ingredients.map(ing => {
+        return <span
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}
+            key={ing.name}>
+            {ing.name} ({ing.amount})
+        </span>
+    })
+
+    return (
+        <div className='Order'>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default order;
