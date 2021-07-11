ee4b82d88ef8c9afdbb2b7ffdc460cb9280b7bf5
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-12 00:31:06 +0530

    Fixed the Order Now button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends React.Component {
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false
     }
@@ -32,7 +31,7 @@ class BurgerBuilder extends React.Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({ purchasable: sum > 0 })
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -75,7 +74,7 @@ class BurgerBuilder extends React.Component {
                         disabled={disabledInfo}
                         price={this.props.price}
                         ordered={this.purchaseHandler}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                     />
                 </Aux>
             );
