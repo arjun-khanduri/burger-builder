1b6c064c03317b4f8cdcea64b1948cff6b605001
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-22 01:23:48 +0530

    Added logout functionality

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -16,6 +16,20 @@ export const authSuccess = (token, userId) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT,
+    }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
 export const authFail = (error) => {
     return {
         type: actionTypes.AUTH_FAIL,
@@ -32,15 +46,16 @@ export const auth = (email, password, isSignup) => {
             returnSecureToken: true
         }
         let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCIBcALnjNuz8Uf93i2A5ie50KI2FXa17s';
-        if(!isSignup){
+        if (!isSignup) {
             url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCIBcALnjNuz8Uf93i2A5ie50KI2FXa17s';
         }
         axios.post(url, authData)
-        .then(response => {
-            dispatch(authSuccess(response.data.idToken, response.data.userId));
-        })
-        .catch(err => {
-            dispatch(authFail(err.response.data.error));
-        });
+            .then(response => {
+                dispatch(authSuccess(response.data.idToken, response.data.userId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                dispatch(authFail(err.response.data.error));
+            });
     };
 };
