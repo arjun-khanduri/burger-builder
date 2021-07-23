cd93145d51999dd62103cf42c353456aa819e38a
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-23 23:50:26 +0530

    Redirecting user to Checkout page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -41,6 +41,11 @@ class Auth extends React.Component {
         isSignup: true
     };
 
+    componentDidMount() {
+        if (!this.props.buildingBurger && this.props.authRedirectPath !== '/')
+            this.props.onSetAuthRedirectPath();
+    }
+
     checkValidity = (value, rules) => {
         let isValid = true;
         if (rules.required)
@@ -109,7 +114,7 @@ class Auth extends React.Component {
 
         let authRedirect = null;
         if (this.props.isAuthenticated)
-            authRedirect = <Redirect to="/" />;
+            authRedirect = <Redirect to={this.props.authRedirectPath} />;
 
         return (
             <div className="Auth">
@@ -133,12 +138,15 @@ const mapStateToProps = (state) => {
         loading: state.auth.loading,
         error: state.auth.error,
         isAuthenticated: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     }
 }
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,8 +33,10 @@ class BurgerBuilder extends React.Component {
     purchaseHandler = () => {
         if (this.props.isAuthenticated)
             this.setState({ purchasing: true });
-        else
+        else {
+            this.props.onSetAuthRedirectPath('/checkout');
             this.props.history.push('/auth');
+        }
     }
 
     purchaseCancelHandler = () => {
@@ -101,7 +103,8 @@ const mapDispatchToProps = dispatch => {
         onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
         onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
         onInitIngredients: () => dispatch(actions.initIngredients()),
-        onInitPurchase: () => dispatch(actions.purchaseInit())
+        onInitPurchase: () => dispatch(actions.purchaseInit()),
+        onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path)),
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
