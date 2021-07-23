831898dd8bc7a1783eddb9dce7e214e6d03fa9c1
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-07-24 01:13:58 +0530

    Fixed a bug

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -59,7 +59,7 @@ export const auth = (email, password, isSignup) => {
                 localStorage.setItem('token', response.data.idToken);
                 localStorage.setItem('expirationDate', expirationDate);
                 localStorage.setItem('userId', response.data.localId);
-                dispatch(authSuccess(response.data.idToken, response.data.userId));
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
