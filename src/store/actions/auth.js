b1a6dd010ab9e69e696512287f8b5dc201b3a44f
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-10-01 23:37:14 +0530

    Put the API key in an env file

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import API from '../../API';
 
 import * as actionTypes from './actionTypes';
 
@@ -49,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         }
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + process.env.REACT_APP_API;
         if (!isSignup) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + API;
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + process.env.REACT_APP_API;
         }
         axios.post(url, authData)
             .then(response => {
